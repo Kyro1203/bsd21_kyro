00dc2a3e258c3090e6a5cb257af9c87d93b91259
Kyrillos Habib <s50709@edu.campus02.at>
2022-05-31 19:44:49 +0200

    exercise 6

--- src/main/java/at/campus02/bsd/calculator.java
+++ src/main/java/at/campus02/bsd/calculator.java
@@ -5,21 +5,47 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
 
+/**
+ * @author Habib Kyrillos
+ */
+
 public class calculator {
 
     private static Logger logger = LogManager.getLogger();
 
     public static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Adds two numbers
+     * @param UsersInput first number
+     * @param UsersInput1 second number
+     * @return Value of first number added with the second number
+     */
 
     public static double add(double UsersInput, double UsersInput1){
         logger.debug(UsersInput + "+" + UsersInput1);
         return UsersInput + UsersInput1;
     }
+
+    /**
+     * Subtract two numbers
+     * @param UsersInput first number
+     * @param UsersInput1 second number
+     * @return Value of first number minus with the second number
+     */
+
     public static double minus(double UsersInput, double UsersInput1){
         logger.debug(UsersInput + "-" + UsersInput1);
         return UsersInput - UsersInput1;
     }
+
+    /**
+     * divide two numbers
+     * @param UsersInput first number
+     * @param UsersInput1 second number
+     * @return Value of first number divided by the second number
+     */
+
     public static double divide(double UsersInput, double UsersInput1){
         logger.debug(UsersInput + "/" + UsersInput1);
         if (UsersInput1 == 0){
@@ -30,10 +56,25 @@ public class calculator {
             return UsersInput / UsersInput1;
         }
     }
+
+    /**
+     * multiply two numbers
+     * @param UsersInput first number
+     * @param UsersInput1 second number
+     * @return Value of first number multiplied by the second number
+     */
+
     public static double multiply(double UsersInput, double UsersInput1){
         logger.debug(UsersInput + "*" + UsersInput1);
         return UsersInput * UsersInput1;
     }
+
+    /**
+     * faculty of a number
+     * @param Usersinput first number
+     * @return Value of first number multiplied with the same number - 1 until Usersinput is 1
+     */
+
     public static double fakultaet(double Usersinput){
         logger.debug(Usersinput + "!");
         if (Usersinput==1){
